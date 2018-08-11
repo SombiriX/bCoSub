e5c2d466744982b138abed06683fb4af15e92474
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-08-11 04:49:14 +0000

    Vue app loads and talks to django api

--- bCoreRiskSite/bCoreRiskApp/bcore-frontend/src/App.vue
+++ bCoreRiskSite/bCoreRiskApp/bcore-frontend/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <v-app dark>
-    <v-toolbar color="primary">
-      <v-toolbar-items>
-        <v-btn small color="secondary">Manage Risks</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-btn color="primary" dark>Add Risk</v-btn>
     <v-content>
-      <router-view/>
+      <v-toolbar color="primary">
+        <v-toolbar-items>
+          <v-btn small color="secondary">Manage Risks</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-btn color="primary" dark>Add Risk</v-btn>
     </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
-
+  name: 'riskApp'
 }
 </script>
--- bCoreRiskSite/bCoreRiskApp/bcore-frontend/src/main.js
+++ bCoreRiskSite/bCoreRiskApp/bcore-frontend/src/main.js
@@ -1,10 +1,88 @@
 import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
-import App from './App.vue'
+import './plugins/bootstrap-vue'
+import './plugins/vue-resource'
+import riskApp from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 new Vue({
-  render: h => h(App)
+  router,
+  render: h => h(riskApp),
+  delimiters: ['${', '}'],
+  data: {
+    risks: [],
+    loading: false,
+    currentRisk: {},
+    message: null,
+    newRisk: { 'risk_class': null }
+  },
+  mounted: function () {
+    this.getRisks()
+  },
+  methods: {
+    getRisks: function () {
+      this.loading = true
+      this.$http.get('/api/risk/')
+        .then((response) => {
+          this.risks = response.data
+          this.loading = false
+        })
+        .catch((err) => {
+          this.loading = false
+          console.log(err)
+        })
+    },
+    getRisk: function (id) {
+      this.loading = true
+      this.$http.get(`/api/risk/${id}/`)
+        .then((response) => {
+          this.currentRisk = response.data
+          this.loading = false
+        })
+        .catch((err) => {
+          this.loading = false
+          console.log(err)
+        })
+    },
+    addRisk: function () {
+      this.loading = true
+      this.$http.post('/api/risk/', this.newRisk)
+        .then((response) => {
+          this.loading = false
+          this.getRisks()
+        })
+        .catch((err) => {
+          this.loading = false
+          console.log(err)
+        })
+    },
+    updateRisk: function () {
+      this.loading = true
+      this.$http.put(`/api/risk/${this.currentRisk.risk_id}/`, this.currentRisk)
+        .then((response) => {
+          this.loading = false
+          this.currentRisk = response.data
+          this.getRisks()
+        })
+        .catch((err) => {
+          this.loading = false
+          console.log(err)
+        })
+    },
+    deleteRisk: function (id) {
+      this.loading = true
+      this.$http.delete(`/api/risk/${id}/`)
+        .then((response) => {
+          this.loading = false
+          this.getRisks()
+        })
+        .catch((err) => {
+          this.loading = false
+          console.log(err)
+        })
+    }
+  }
 }).$mount('#app')
