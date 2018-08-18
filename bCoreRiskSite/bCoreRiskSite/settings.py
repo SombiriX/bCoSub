738721449fe83e4e858bf22e88e8aca6ea5a0927
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-08-18 06:31:23 +0000

    Setting to push static files to AWS

--- bCoreRiskSite/bCoreRiskSite/settings.py
+++ bCoreRiskSite/bCoreRiskSite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'webpack_loader',
     'django_extensions',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,15 @@ WEBPACK_LOADER = {
         'IGNORE': ['.+\.hot-update.js', '.+\.map']
     }
 }
+
+# AWS settings
+
+AWS_STORAGE_BUCKET_NAME = 'zappa-bcobucket1'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_S3_HOST = 's3-us-west-1.amazonaws.com'
